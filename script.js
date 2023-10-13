df3fcd5165f9a6f45fd323938931284cd5ba6731
Chris Rice <chrisrice2442@yahoo.com>
2023-10-13 16:26:34 -0400

    adding questions in my javascript, as well as commented out a timer i plan to incorporate in the future

---
+++ script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        question: "Who is Spider-Man behind the mask?",
+        answers: [
+            {text: "A. Bruce Banner", correct: false}
+            {text: "B. Peter Piper", correct: false}
+            {text: "C. Peter Frampton", correct: false}
+            {text: "D. Peter Parker", correct: true},
+        ]
+    },
+    {
+        question: "What animal bit Spider-Man to grant him with his superpowers?"
+        answers: [
+            {text: "A. Cockroach"}
+        ]
+    }
+]
+
+
+// timer function!
+// var i = 5;
+// var poop = setInterval(function(){
+//     console.log(i);
+//     i--;
+//     if (i < 0) {
+//         clearInterval(poop)
+//     }    
+// }, 1000);
