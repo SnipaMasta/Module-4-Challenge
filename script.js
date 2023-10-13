f8011f563fa94d7a03f514ae349c022c1d1ad7c4
Chris Rice <chrisrice2442@yahoo.com>
2023-10-13 16:37:47 -0400

    finished adding all quesitions

--- script.js
+++ script.js
@@ -2,16 +2,47 @@ var questions = [
     {
         question: "Who is Spider-Man behind the mask?",
         answers: [
-            {text: "A. Bruce Banner", correct: false}
-            {text: "B. Peter Piper", correct: false}
-            {text: "C. Peter Frampton", correct: false}
-            {text: "D. Peter Parker", correct: true},
+            { text: "A. Bruce Banner", correct: false }
+            { text: "B. Peter Piper", correct: false }
+            { text: "C. Peter Frampton", correct: false }
+            { text: "D. Peter Parker", correct: true },
         ]
     },
     {
         question: "What animal bit Spider-Man to grant him with his superpowers?"
         answers: [
-            {text: "A. Cockroach"}
+            { text: "A. Cockroach", correct: false }
+            { text: "B. Spider", correct: true }
+            { text: "C. Mosquito", correct: false }
+            { text: "D. Giraffe", correct: false }
+        ]
+    },
+    {
+        question: "Which duo created the character of Spider-Man?"
+        answers: [
+            { text: "A. Stanley Tucci & Steve Ditko ", correct: false }
+            { text: "B. Stan Lee & Steve Ditko", correct: true }
+            { text: "C. Stanley Kubrick & Steve Ditko", correct: false }
+            { text: "D. Stanley Yelnats & Steve Ditko", correct: false }
+
+        ]
+    },
+    {
+        question: "Which of these is NOT a Spider-Man villain?"
+    answers: [
+            { text: "A. Demon", correct: true }
+        { text: "B. Sandman", correct: false }
+        { text: "C. Venom", correct: false }
+        { text: "D. Kraven", correct: false }
+        ]
+    },
+    {
+        question: "What year was the first issue of 'Amazing Spider-Man released?"
+        answers: [
+            { text: "A. 1959", correct: false }
+            { text: "B. 1966", correct: false }
+            { text: "C. 1970", correct: false }
+            { text: "D. 1963", correct: true }
         ]
     }
 ]
@@ -24,5 +55,5 @@ var questions = [
 //     i--;
 //     if (i < 0) {
 //         clearInterval(poop)
-//     }    
+//     }
 // }, 1000);
